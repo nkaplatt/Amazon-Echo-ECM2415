2b5b4ab1f087d06a6287acfc79f286f06eb1c33c
tillyshirley <tvs201@exeter.ac.uk>
2017-02-21 17:08:05 +0000

    on button plays hello tune - will try find a better hell noise

--- OnOff.java
+++ OnOff.java
@@ -7,50 +7,53 @@ import java.awt.event.*;
  */
 public class OnOff{
 	public static void main(String[] args){
-    //Set up container.
+		//Set up container.
 		JFrame frame = new JFrame("Amazon Echo");
-    frame.setTitle("Welcome to Amazon Echo!");
+		frame.setTitle("Welcome to Amazon Echo!");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    frame.setLocationRelativeTo(null);
-    frame.setSize(500,500);
+		frame.setLocationRelativeTo(null);
+		frame.setSize(500,500);
 		frame.setVisible(true);
 
-    //Set up components.
+		//Set up components.
 		JPanel panel = new JPanel();
 		JButton onButton = new JButton("On");
 		JButton offButton = new JButton("Off");
-    frame.add(panel);
+		frame.add(panel);
 		panel.add(onButton);
 		panel.add(offButton);
 		onButton.addActionListener(new OnAction());
 		offButton.addActionListener(new OffAction());
-  }
+	}
 
 	static class OnAction implements ActionListener{
-    //Create new frame when action performed
+		//Create new frame when action performed
 		public void actionPerformed(ActionEvent e){
 			JFrame frame2 = new JFrame("");
-      frame2.setLocationRelativeTo(null);
+			frame2.setLocationRelativeTo(null);
 			frame2.setVisible(true);
 			frame2.setSize(100,80);
 			JLabel label = new JLabel("Echo is now on!");
 			JPanel panel = new JPanel();
 			frame2.add(panel);
 			panel.add(label);
+			ButtonNoise.main(null);
+
+
 		}
 	}
 
-  static class OffAction implements ActionListener{
-    //Create new frame when action performed
-    public void actionPerformed(ActionEvent e){
-      JFrame frame2 = new JFrame("");
-      frame2.setLocationRelativeTo(null);
-      frame2.setVisible(true);
-      frame2.setSize(100,80);
-      JLabel label = new JLabel("Echo is now Off!");
-      JPanel panel = new JPanel();
-      frame2.add(panel);
-      panel.add(label);
-    }
-  }
+	static class OffAction implements ActionListener{
+		//Create new frame when action performed
+		public void actionPerformed(ActionEvent e){
+			JFrame frame2 = new JFrame("");
+			frame2.setLocationRelativeTo(null);
+			frame2.setVisible(true);
+			frame2.setSize(100,80);
+			JLabel label = new JLabel("Echo is now Off!");
+			JPanel panel = new JPanel();
+			frame2.add(panel);
+			panel.add(label);
+		}
+	}
 }
