b45e222f07b5c9a1a92ecf6c74e1956231b030b6
tillyshirley <tillyshirley@users.noreply.github.com>
2017-03-07 12:32:41 +0000

    Add files via upload

---
+++ SearchText.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+
+/**
+ * Created by tilz on 03/03/2017.
+ */
+
+
+public class SearchText{
+    static boolean findWakeWord (String text ){
+       String WakeWord = "Alexa";
+       boolean found = Arrays.asList(text.split(" ")).contains(WakeWord);
+       if(found){
+           System.out.println("wakeword found");
+           //enter text after wakeword
+           String [] parts = text.split(WakeWord);
+           String question = parts[1];
+           System.out.println(question);
+       }
+       else{
+           //keep listening
+       }
+       return found ;
+    }
+    public static void main (String[] argv){
+        findWakeWord("Alexa what is the time?");
+
+    }
+
+}
---
+++ SearchTextTest.java
@@ -0,0 +1,28 @@
+import junit.framework.TestCase;
+
+/**
+ * Created by tilz on 06/03/2017.
+ */
+public class SearchTextTest extends TestCase {
+    public String textTrue;
+    public String textFalse;
+    public void setUp() throws Exception {
+        super.setUp();
+        textTrue = "Alexa what is the time?";
+        textFalse = "Hi tilly how are you?";
+
+    }
+
+    public void tearDown() throws Exception {
+        textTrue = null;
+        textFalse = null;
+    }
+
+    public void testFindWakeWord() throws Exception {
+        assertEquals(true, SearchText.findWakeWord(textTrue));
+        assertEquals(false, SearchText.findWakeWord(textFalse));
+        tearDown();
+    }
+
+
+}
