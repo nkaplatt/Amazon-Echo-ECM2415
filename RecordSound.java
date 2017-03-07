995c270a7a5cce0fb3894575c1cb1027027be923
garethzeta <garethzeta@outlook.com>
2017-03-07 15:04:46 +0000

    Add files via upload

---
+++ RecordSound.java
@@ -0,0 +1,102 @@
+import java.io.File;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import javax.sound.sampled.AudioFileFormat;
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.DataLine;
+import javax.sound.sampled.TargetDataLine;
+/*
+ * Record sound.
+ */
+class RecordSound {
+    private static final String  FILENAME        = "Question.wav";
+    private static final int     TIMER           = 10;     /* secs */
+    private static final int     SAMPLE_RATE     = 16000; /* MHz  */
+    private static final int     SAMPLE_SIZE     = 16;    /* bits */
+    private static final int     SAMPLE_CHANNELS = 1;     /* mono */
+
+    /*
+     * Set up stream.
+     */
+    static AudioInputStream setupStream() {
+        try {
+            AudioFormat af =
+                    new AudioFormat( SAMPLE_RATE
+                            , SAMPLE_SIZE
+                            , SAMPLE_CHANNELS
+                            , true /* signed */
+                            , true /* little-endian */
+                    );
+            DataLine.Info    info = new DataLine.Info( TargetDataLine.class, af );
+            TargetDataLine   line = (TargetDataLine) AudioSystem.getLine( info );
+            AudioInputStream stm  = new AudioInputStream( line );
+            line.open( af );
+            line.start();
+            return stm;
+        } catch ( Exception ex ) {
+            System.out.println( ex ); System.exit( 1 ); return null;
+        }
+    }
+
+    /*
+     * Read stream.
+     */
+    static ByteArrayOutputStream readStream( AudioInputStream stm ) {
+        try {
+            ByteArrayOutputStream bos = new ByteArrayOutputStream();
+
+            int  bufferSize = SAMPLE_RATE * stm.getFormat().getFrameSize();
+            byte buffer[]   = new byte[ bufferSize ];
+
+            for ( int counter = TIMER; counter > 0; counter-- ) {
+                int n = stm.read( buffer, 0, buffer.length );
+                if ( n > 0 ) {
+                    bos.write( buffer, 0, n );
+                } else {
+                    break;
+                }
+            }
+
+            return bos;
+        } catch ( Exception ex ) {
+            System.out.println( ex ); System.exit( 1 ); return null;
+        }
+    }
+
+    /*
+     * Record sound.
+     */
+    static void recordSound( String name, ByteArrayOutputStream bos ) {
+        try {
+            AudioFormat af =
+                    new AudioFormat( SAMPLE_RATE
+                            , SAMPLE_SIZE
+                            , SAMPLE_CHANNELS
+                            , true /* signed */
+                            , true /* little-endian */
+                    );
+            byte[]           ba   = bos.toByteArray();
+            InputStream      is   = new ByteArrayInputStream( ba );
+            AudioInputStream ais  = new AudioInputStream( is, af, ba.length );
+            File             file = new File( name );
+            AudioSystem.write( ais, AudioFileFormat.Type.WAVE, file );
+        } catch ( Exception ex ) {
+            System.out.println( ex ); System.exit( 1 );
+        }
+    }
+
+    /*
+     * Record sound.
+     */
+    public static void main( String[] argv ) {
+        AudioInputStream stm = setupStream();
+        recordSound( FILENAME, readStream( stm ) );
+        //
+        // try running converttotext using file made from recordsound
+        // cant remember how to run it
+        SpeechToText converttotext = new SpeechToText();
+    }
+}
---
+++ SpeechToText.java
@@ -0,0 +1,87 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.DataInputStream;
+import java.util.UUID;
+/*
+ * Speech to text conversion using Microsoft Cognitive Services.
+ *
+ * See http://www.microsoft.com/cognitive-services/en-us/speech-api
+
+ */
+public class SpeechToText {
+    final static String LANG  = "en-US";
+    final static String INPUT = "Question.wav";
+
+    final static String KEY1 = "b750f20fdc0c4ac8aaf59073d98c1a05"; // need our key here!
+  /* final static String KEY2 = "ea072146f15446ed89d1c9f2498c0d87"; */
+
+    /*
+     * Renew an access token --- they expire after 10 minutes.
+     */
+    static String renewAccessToken( String key1 ) {
+        final String method = "POST";
+        final String url =
+                "https://api.cognitive.microsoft.com/sts/v1.0/issueToken";
+        final byte[] body = {};
+        final String[][] headers
+                = { { "Ocp-Apim-Subscription-Key", key1                          }
+                , { "Content-Length"           , String.valueOf( body.length ) }
+        };
+        byte[] response = HttpConnect.httpConnect( method, url, headers, body );
+        return new String( response );
+    }
+
+    /*
+     * Recognize speech.
+     */
+    static String recognizeSpeech( String token, byte[] body ) {
+        final String method = "POST";
+        final String url
+                = ( "https://speech.platform.bing.com/recognize"
+                + "?" + "version"    + "=" + "3.0"
+                + "&" + "format"     + "=" + "json"
+                + "&" + "device.os"  + "=" + "wp7"
+                + "&" + "scenarios"  + "=" + "smd"
+                + "&" + "locale"     + "=" + LANG
+                + "&" + "appid"      + "=" + "D4D52672-91D7-4C74-8AD8-42B1D98141A5"
+                + "&" + "instanceid" + "=" + UUID.randomUUID().toString()
+                + "&" + "requestid"  + "=" + UUID.randomUUID().toString()
+        );
+        final String[][] headers
+                = { { "Content-Type"   , "audio/wav; samplerate=16000"  }
+                , { "Content-Length" , String.valueOf( body.length )  }
+                , { "Authorization"  , "Bearer " + token              }
+        };
+        byte[] response = HttpConnect.httpConnect( method, url, headers, body );
+        return new String( response );
+    }
+
+    /*
+     * Read data from file.
+     */
+    static byte[] readData( String name ) {
+        try {
+            File            file = new File( name );
+            FileInputStream fis  = new FileInputStream( file );
+            DataInputStream dis  = new DataInputStream( fis );
+            byte[] buffer = new byte[ (int) file.length() ];
+            dis.readFully( buffer );
+            dis.close();
+            return buffer;
+        } catch ( Exception ex ) {
+            System.out.println( ex ); System.exit( 1 ); return null;
+        }
+    }
+
+    /*
+     * Convert speech to text.
+     */
+    public static void main( String[] argv ) {
+        final String token  = renewAccessToken( KEY1 );
+        final byte[] speech = readData( INPUT );
+        //
+        final String text   = recognizeSpeech( token, speech ); // for Tillys part
+        //
+        System.out.println( text );
+    }
+}
